6a4a09b564c16760ef4f50287625bec9b2dfd0b1
dossancto <dossantossilvalucas963@gmail.com>
2026-05-04 18:06:08 -0300

    init project

---
+++ src/main/java/com/dossancto/todo/dossancto/DossanctoApplication.java
@@ -0,0 +1,13 @@
+package com.dossancto.todo.dossancto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossanctoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossanctoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dossancto/todo/dossancto/DossanctoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dossancto.todo.dossancto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossanctoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
